726de9677b8060b68688503689bd220d861b63d8
Ibrohimjon <primexstop@gmail.com>
2025-03-26 01:03:33 +0500

    unsplash

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import { sendEmailVerification } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
@@ -6,24 +6,69 @@ import { toast } from "react-toastify";
 
 function Profile() {
   const { user } = useGlobalContext();
-
+  const [imageBase64, setImageBase64] = useState(null);
   const sendVerification = () => {
     sendEmailVerification(auth.currentUser, {
-      url: "http://localhost:5173/profile",
+      url: "https://ik-unsplash.vercel.app/profile",
     }).then(() => {
       toast.success("Verification email is sended !");
     });
   };
 
+  const imageChangeBase64 = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    if (file.size / 1024 < 1024) {
+      reader.addEventListener("load", () => {
+        setImageBase64(reader.result);
+      });
+      reader.readAsDataURL(file);
+    } else {
+      toast.warn("Oops! The image must be Less than 1MB");
+    }
+  };
+  const cancelImageSave = () => {
+    setImageBase64(null);
+  };
+
+  const saveImage = () => {
+    setImageBase64(imageBase64);
+  };
+
   return (
     <div className="align-elements py-10">
-      <div className="flex gap-5">
-        <div>
+      <div className="flex flex-col gap-5 md:flex-row">
+        <div className="flex flex-col items-center justify-center">
           <img
-            src={user.photoURL || "User Photo"}
+            src={(imageBase64 ?? user.photoURL) || "User Photo"}
             alt={user.displayName + "avatar "}
-            className="h-40 w-40 rounded-full"
+            className="mb-5 h-40 w-40 rounded-full object-cover md:h-40 md:w-40"
           />
+
+          {!imageBase64 && (
+            <input
+              onChange={imageChangeBase64}
+              type="file"
+              accept=".jpg,.jpeg,.png.,gif"
+              className="file-input file-input-primary file-input-sm"
+            />
+          )}
+          {imageBase64 && (
+            <div className="flex w-full justify-center gap-1">
+              <button
+                onClick={cancelImageSave}
+                className="btn btn-secondary btn-xs grow"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={saveImage}
+                className="btn btn-primary btn-xs grow"
+              >
+                Save
+              </button>
+            </div>
+          )}
         </div>
         <div className="bg-base-200 grid grow grid-cols-1 gap-5 rounded-lg p-5 md:grid-cols-2">
           <h2>
